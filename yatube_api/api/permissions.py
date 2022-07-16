8db9a47681f6d6549c4101eabaf538aa110e7e2c
Герман Кабачков <german_kabachkov@mail.ru>
2022-07-17 00:46:52 +0300

    fifth

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -12,5 +12,5 @@ class AuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             obj.author == request.user
-            or request.method in permissions.SAFE_METHOD
+            or request.method in permissions.SAFE_METHODS
         )
